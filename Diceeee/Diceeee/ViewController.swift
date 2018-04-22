bf1ba90f006dced7a04ee5a186a378005c919576
XingLiu <xingliu14@gmail.com>
2018-04-21 23:29:23 -0500

    init Diceeee

---
+++ Diceeee/Diceeee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diceeee
+//
+//  Created by Xing Liu on 21/4/2018.
+//  Copyright © 2018 Xing Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
